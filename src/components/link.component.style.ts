3bd57030275d36c31051a03a4ded6a9238e7611a
Michel Elesbão <michel.elesbao@taqtile.com.br>
2020-11-16 20:49:07 -0300

    link color

--- src/components/link.component.style.ts
+++ src/components/link.component.style.ts
@@ -1,8 +1,13 @@
 import styled from "styled-components";
+import { Color, Transition } from "./constants";
 
 export const Link = styled.a`
   text-decoration: none;
+  color: inherit;
+  transition: color ${Transition.Fast};
   &:hover {
     cursor: pointer;
+    text-decoration: underline;
+    color: ${Color.Primary};
   }
 `;
