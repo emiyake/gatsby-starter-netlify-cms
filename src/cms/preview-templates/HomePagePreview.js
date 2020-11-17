cc523361155c26288ca0f7309b150df38a88d7c2
Michel Elesbão <michel.elesbao@taqtile.com.br>
2020-11-17 13:34:19 -0300

    adding home page

---
+++ src/cms/preview-templates/HomePagePreview.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Home } from '../../pages/home'
+
+const HomePagePreview = ({ entry, getAsset }) => {
+  const data = entry.getIn(['data']).toJS()
+
+  if (data) {
+    return (
+      <Home
+      />
+    )
+  } else {
+    return <div>Loading...</div>
+  }
+}
+
+IndexPagePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  getAsset: PropTypes.func,
+}
+
+export default IndexPagePreview
